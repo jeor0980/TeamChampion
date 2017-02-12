5ab12e78e3f170f8430238d8d95e55b9348e9078
haydenreich <hayden.reich@colorado.edu>
2017-02-11 23:21:55 -0700

    Fixed it so it actually kind of works, instead of not working at all

---
+++ naive_README.md
@@ -0,0 +1,4 @@
+How to run: python2.7 naive_alg.py
+  -takes no parameters
+
+However, it does assume that you have a properly set up database
--- naive_alg.py
+++ naive_alg.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python
+"""
+So, what's going on here?
+Refer to the naive_README for instructions!
+This is definitely not the best solution...
+"""
 from pymongo import MongoClient
 import pprint
 from bson.json_util import dumps
@@ -9,51 +14,50 @@ GROUP_SIZE = 3
 def recursiveSort(students, groups, group_cursor, small_group_name):
     id = group_cursor['_id']
     group = groups.find_one({'_id' : id})
-    print "Checking group: " + group['group_name']
     index = len(group['members']) - 1
-    print str(index + 1) + " members"
+
     if index > 2:
-        student_obj = students.find_one({
-            'second_pref' : small_group_name
-        })
-        while 'second_pref_selected' in student_obj:
-            index -= 1
+        while index >= 0:
             student_obj = students.find_one({
+                'second_pref' : small_group_name,
                 'student_name' : group['members'][index]
             })
-
-        print group['members']
-        print student_obj['student_name']
-        groups.find_one_and_update(
-            {'_id' : id}, {'$pull' : {'members' : student_obj['student_name']}}
-        )
-        print groups.find_one({'_id' : id})['members']
-        groups.find_one_and_update(
-            {'group_name' : student_obj['second_pref']}, {'$push' : {'members' : student_obj['student_name']}}
-        )
-        students.update_one({'_id' : student_obj['_id']}, {'$set' :
+            if student_obj:
+                index = -1
+            else:
+                index -= 1
+        if student_obj:
+            groups.find_one_and_update(
+                {'_id' : id}, {'$pull' : {'members' : student_obj['student_name']}}
+            )
+            groups.find_one_and_update(
+                {'group_name' : student_obj['second_pref']}, {'$push' : {'members' : student_obj['student_name']}}
+            )
+            students.update_one({'_id' : student_obj['_id']}, {'$set' :
                                                            {'second_pref_selected'
                                                             : 'True'}})
 
-        group_min = 100
-        group_max = 0
-        small_group_name = ""
-        large_group_name = ""
-        raw_input("press enter to continue...")
-        for group in groups.find():
-            if len(group['members']) < group_min:
-                small_group_name = group['group_name']
-                group_min = len(group['members'])
-            if len(group['members']) > group_max:
-                large_group_name = group['group_name']
-                group_max = len(group['members'])
-        recursiveSort(students, groups, group, small_group_name)
+            group_min = 100
+            group_max = 0
+            small_group_name = ""
+            large_group_name = ""
+            for group in groups.find():
+                if len(group['members']) < group_min:
+                    small_group_name = group['group_name']
+                    group_min = len(group['members'])
+                if len(group['members']) > group_max:
+                    large_group_name = group['group_name']
+                    group_max = len(group['members'])
+            recursiveSort(students, groups, group, small_group_name)
+        else:
+            return
     else:
         return
 
 def sortThemBitches(students, groups):
-    print "running"
+    print "running..."
     groups.update({}, {'$unset' : {'members' : 1}}, multi=True)
+    students.update({}, {'$unset' : {'second_pref_selected' : 1}}, multi=True)
     for student in students.find():
         pprint.pprint(student["student_name"])
         print "adding " + student["student_name"] + " to group " \
@@ -62,6 +66,7 @@ def sortThemBitches(students, groups):
                                    {'$push' : {'members' :
                                                student['student_name']}})
     print "finished first pass!"
+    print "Hmmm, difficult... VERY difficult. Where to put you?"
     print "---------------------------------------------------"
     group_min = 100
     group_max = 0
@@ -75,6 +80,16 @@ def sortThemBitches(students, groups):
             large_group_name = group['group_name']
             group_max = len(group['members'])
         recursiveSort(students, groups, group, small_group_name)
+    for group in groups.find():
+        if len(group['members']) < group_min:
+            small_group_name = group['group_name']
+            group_min = len(group['members'])
+        if len(group['members']) > group_max:
+            large_group_name = group['group_name']
+            group_max = len(group['members'])
+    recursiveSort(students, groups, group, small_group_name)
+    print "---------------------------------------------------"
+    print "Well if you're sure, better be... GRYFFINDOR"
 
 def main(args):
     
