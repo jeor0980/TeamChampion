3a0d717ebb93ecc5e4dc44ccca4c24125104f965
Hayden Reich <hayden.reich@colorado.edu>
2017-02-13 00:39:21 -0700

    Added source for website
    all python scripts, and a list of required extensions/libraries etc

---
+++ SortingHat/config.py
@@ -0,0 +1,22 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this_is_a_secret_key'
+
+class ProductionConfig(Config):
+    DEBUG = FALSE
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    Testing = True
