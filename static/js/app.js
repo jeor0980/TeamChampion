0a5a7b63d968200cb588040576b82f3d36cc3444
Jesus Ortiz Tovar <jeor0980@colorado.edu>
2017-03-12 23:27:56 -0600

    Added Basic Angular Functionality to the site
    Added some extra script directories under /js to better see how the app
    works.
    
    To run this small demo:
    
    1. Go to the TeamChampion directory and inside call "python -m
    SimpleHTTTPServer"
    2. Go to http://localhost:8000
    3. Take it all in

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+'use strict'; //interpreted as script mode
+
+// create the module and name it sortingApp
+// also include ngRoute for all our routing needs
+var sortingApp = angular.module('sortingApp', ['ngRoute']);
