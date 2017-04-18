ade28d8565c06428c28b81754e01140de8fb40cb
Jesus Ortiz Tovar <jeor0980@colorado.edu>
2017-04-18 01:11:37 -0600

    Dynamic Navigation Bar 90% Complete
    Missing Redirect after user signs in

--- hatServer/static/js/app.js
+++ hatServer/static/js/app.js
@@ -2,18 +2,43 @@
 
 // create the module and name it sortingApp
 // also include ngRoute for all our routing needs
-var sortingApp = angular.module('sortingApp', ['ngRoute']);
+var sortingApp = angular.module('sortingApp', ['ngRoute', 'ngSanitize']);
+
+
+sortingApp.directive('compile', ['$compile', function ($compile) {
+    return function(scope, element, attrs) {
+        scope.$watch(
+            function(scope) {
+                // watch the 'compile' expression for changes
+                return scope.$eval(attrs.compile);
+            },
+            function(value) {
+                // when the 'compile' expression changes
+                // assign it into the current DOM
+                element.html(value);
+
+                // compile the new DOM and link it to the current
+                // scope.
+                // NOTE: we only compile .childNodes so that
+                // we don't get into infinite loop compiling ourselves
+                $compile(element.contents())(scope);
+            }
+        );
+    };
+}])
+
 
 sortingApp.service('userInformation', function () {
-    
-    var id = '';
-    var fullName = 'placeholderfullname';
-    var givenName = '';
-    var familyName = '';
-    var email = '';
-    var idToken = '';
-    var imageUrl = '';
-    var authenticationInstance = '';
+    var isLoggedIn = false;
+    var id = null;
+    var fullName = null;
+    var givenName = null;
+    var familyName = null;
+    var email = null;
+    var idToken = null;
+    var imageUrl = null;
+    var authenticationInstance = null;
+
 
   return {
 
@@ -69,6 +94,25 @@ sortingApp.service('userInformation', function () {
       },
       setAuthInstance: function(value) {
           authenticationInstance = value;
+      },
+      getIsLoggedIn: function () {
+          return isLoggedIn;
+      },
+      setIsLoggedIn: function(value) {
+          isLoggedIn = value;
+      },
+
+      destroy: function() {
+      var isLoggedIn = false;
+      var id = null;
+      var fullName = null;
+      var givenName = null;
+      var familyName = null;
+      var email = null;
+      var idToken = null;
+      var imageUrl = null;
+      var authenticationInstance = null;
+      console.log("Session was destroyed");
       },
   };
 });
